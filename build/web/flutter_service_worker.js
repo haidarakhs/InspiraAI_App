9339bc63b75b6da3dade8727d7cb0f0e854324fa
haidarakhs <haidarakbar.hs@gmail.com>
2025-04-17 09:51:09 +0700

    update lagi

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c09567f57155a2a69df3e64748e23e7e",
+const RESOURCES = {"flutter_bootstrap.js": "168eb8ccd278d15793a1f6745d7161ee",
 "version.json": "f62997c57878208fde9939ca1395d16e",
 "index.html": "6b741c2f86adf950288be96ec40a81b4",
 "/": "6b741c2f86adf950288be96ec40a81b4",
-"main.dart.js": "212665c79116bdeed06d0ed62f48bb4c",
+"main.dart.js": "ae6b1009ccdb789c9aee017a6ec18e14",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
--- lib/gemini_api.dart
+++ lib/gemini_api.dart
@@ -4,18 +4,20 @@ import 'package:http/http.dart' as http;
 class GeminiAPI {
   static Future<String> fetchInspiration() async {
     try {
-      final response = await http.get(Uri.parse('https://zenquotes.io/api/random'));
+      // Panggil zenquotes lewat proxy allorigins.win
+      final response = await http.get(
+        Uri.parse('https://api.allorigins.win/get?url=https://zenquotes.io/api/random'),
+      );
+
       if (response.statusCode == 200) {
-        final List<dynamic> data = jsonDecode(response.body);
-        final quote = data[0]['q'];
-        final author = data[0]['a'];
-        return '$quote\n- $author';
+        final jsonData = json.decode(response.body);
+        final originalData = json.decode(jsonData['contents']);
+        return "${originalData[0]['q']} - ${originalData[0]['a']}";
       } else {
         throw Exception('Gagal mengambil inspirasi');
       }
     } catch (e) {
-      print("Error saat fetch: $e");
-      return "Gagal mengambil inspirasi. Silakan coba lagi.";
+      throw Exception('Terjadi kesalahan: $e');
     }
   }
 }
